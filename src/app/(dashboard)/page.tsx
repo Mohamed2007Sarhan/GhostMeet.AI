7d16c27ff79c2c43c6a5887c6679f1e981bbc8d4
Mohamed Sarhan <prof7mohamedsarhan@gmail.com>
2025-07-18 08:22:18 +0300

    chore: cleanup ignored files from git tracking

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,8 +2,12 @@ import { HomeView } from "@/modules/home/ui/views/home-view"
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { caller } from "@/trpc/server";
+
 
 const Page = async () => {
+  // const data = await caller.hello({ text: "Mohamed Server"});
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -11,6 +15,7 @@ const Page = async () => {
   if (!session) {
     redirect("/sign-in")
   }
+  // return <p>{data.greeting}</p>
   return <HomeView/>
 };
 
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const { data: session } = authClient.useSession();
-
-  if (!session) {
-    return (
-      <p>Loading...</p>
-    );
-  }
-
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Mohamed"}));
+ 
   return (
     <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-      <button onClick={() => authClient.signOut()} className="bg-primary text-white px-4 py-2 rounded">Sign Out</button>
+      {data?.greeting}
     </div>
   );
 };
